ced3ef8c9fd0e52c0ac48117bc59ac405f82d69f
Derick Zhang <centuryib100@gmail.com>
2020-12-29 19:50:51 -0800

    Water sensor sketch

---
+++ WaterSensor.ino
@@ -0,0 +1,63 @@
+int GREEN_LED = 13;
+int YELLOW_LED = 12;
+int RED_LED = 11;
+int WATER_SENSOR_PIN = A5;
+int WATER_LEVEL;
+int BUZZER = 8;
+boolean BUZZED = false;
+
+void setup() {
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  Serial.begin(9600);
+  pinMode(BUZZER, OUTPUT);
+}
+
+void dance() {
+  digitalWrite(GREEN_LED, HIGH);
+  digitalWrite(YELLOW_LED, HIGH);
+  digitalWrite(RED_LED, HIGH);
+  delay(1000);
+  digitalWrite(GREEN_LED, LOW);
+  delay(1000);
+  digitalWrite(YELLOW_LED, LOW);
+  delay(1000);
+  digitalWrite(RED_LED, LOW);
+}
+
+void loop() {
+  resetLights();
+  WATER_LEVEL = analogRead(WATER_SENSOR_PIN);
+  Serial.println(WATER_LEVEL);
+  if (WATER_LEVEL <= 400) {
+    BUZZED = false;
+  } else if (WATER_LEVEL <= 500) {
+    digitalWrite(GREEN_LED, HIGH);
+    BUZZED = false;
+  } else if (WATER_LEVEL <= 550) {
+    digitalWrite(YELLOW_LED, HIGH);
+    digitalWrite(RED_LED, HIGH);
+    if (!BUZZED) {
+      tone(BUZZER, 2000);
+      delay(1000);
+      noTone(BUZZER);
+      BUZZED = true;
+    }
+  } else {
+    digitalWrite(RED_LED, HIGH);
+    if (!BUZZED) {
+      tone(BUZZER, 2000);
+      delay(1000);
+      noTone(BUZZER);
+      BUZZED = true;
+    }
+  }
+  delay(1000);
+}
+
+void resetLights() {
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+}
